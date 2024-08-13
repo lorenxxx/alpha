c39eecf1ad3bd7ee419223f090eb0396f2dfc5f3
lorenliang <lorenliang@tencent.com>
2024-08-13 17:49:12 +0800

    init

---
+++ src/main/java/com/clawchat/alpha/web/DemoController.java
@@ -0,0 +1,16 @@
+package com.clawchat.alpha.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @GetMapping()
+    public String demo() {
+        return "ok";
+    }
+
+}
