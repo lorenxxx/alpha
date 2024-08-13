8d606d18510d02413b34898ba73431e69c49576e
lorenliang <lorenliang@tencent.com>
2024-08-13 23:01:19 +0800

    redis

--- src/main/java/com/clawchat/alpha/service/impl/DemoService.java
+++ src/main/java/com/clawchat/alpha/service/impl/DemoService.java
@@ -4,6 +4,7 @@ import com.clawchat.alpha.entity.Demo;
 import com.clawchat.alpha.mapper.IDemoMapper;
 import com.clawchat.alpha.service.IDemoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,8 +13,17 @@ public class DemoService implements IDemoService {
     @Autowired
     private IDemoMapper demoMapper;
 
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
     @Override
     public Demo getDemoByID(Integer id) {
+        String key = "loren";
+        redisTemplate.opsForValue().set(key, "fun");
+
+        String value = redisTemplate.opsForValue().get(key);
+        System.out.printf("value: %s\n", value);
+
         return demoMapper.getByID(id);
     }
 
